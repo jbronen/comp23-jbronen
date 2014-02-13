6646322b5ba70f46f901458701e5955368dbd34d
Jared Bronen <jbronen@gmail.com>
2014-02-12 21:50:55 -0500

    wordcount almost done

--- README.txt
+++
@@ -1,2 +0,0 @@
-Jared Bronen
-COMP 23
--- python/word_count.py
+++ python/word_count.py
@@ -2,3 +2,26 @@
 # Jared Bronen
 # COMP 23 - Lab 2
 
+import sys
+
+if __name__ == '__main__':
+    file = sys.argv[-1]
+
+try:
+    f = open(file, "r")
+except IOError:
+    print "Could not find file"
+    exit()
+    
+a = []
+
+for line in f:
+    for word in line.split():
+      a.append(word.lower())
+      
+a.sort()
+
+for i in a:
+    print i, a.count(i)
+
+print "There are ", len(a), "words in this file"
